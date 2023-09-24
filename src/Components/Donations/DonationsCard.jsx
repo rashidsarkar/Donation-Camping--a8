f3c8d39e4d584eec40b83c1697ad9fa514d59748
rashidsarkar <rashidrock558@gmail.com>
2023-09-25 02:53:20 +0600

    Enhanced DonationsCard component with dynamic styles

--- src/Components/Donations/DonationsCard.jsx
+++ src/Components/Donations/DonationsCard.jsx
@@ -1,13 +1,28 @@
 function DonationsCard({ donationsData }) {
-  const { picture } = donationsData;
+  const { picture, card_bg, category, category_bg, text_button_bg } =
+    donationsData;
+  console.log(card_bg);
+  const cardStyle = {
+    backgroundColor: card_bg,
+  };
+  const textStyle = {
+    backgroundColor: category_bg,
+    color: text_button_bg,
+  };
+
   return (
     <div>
-      <div className="my-5 shadow-xl card w-96 bg-fuchsia-600">
+      <div style={cardStyle} className={`my-5 shadow-xl card w-96 ]`}>
         <figure>
           <img src={picture} alt="Shoes" />
         </figure>
-        <div className="card-body ">
-          <h2 className="card-title">Shoes!</h2>
+        <div className={`card-body inline-block ]`}>
+          <h2
+            style={textStyle}
+            className="inline-block p-2 rounded-lg card-title "
+          >
+            {category}
+          </h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="justify-end card-actions">
             <button className="btn btn-primary">Buy Now</button>
