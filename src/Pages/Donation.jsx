25da0c51d0117d459ea4efa3c4dfefe87cb8d5ca
rashidsarkar <rashidrock558@gmail.com>
2023-09-25 17:14:13 +0600

    Created useFindDataById custom hook for precise data retrieval in DonationDetails component

--- src/Pages/Donation.jsx
+++ src/Pages/Donation.jsx
@@ -1,5 +1,5 @@
 function Donation() {
-  return <div>Donation</div>;
+  return <div>Donasddsadsaation</div>;
 }
 
 export default Donation;
--- src/Pages/DonationDetails.jsx
+++ src/Pages/DonationDetails.jsx
@@ -1,7 +1,15 @@
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { GlobalContext } from "../Layout/MainLayaout";
+import useFindDataById from "../Hooks/UseFinddata";
 
 function DonationDetails() {
+  const contestApiValue = useContext(GlobalContext);
   const { id } = useParams();
+  const foundDonationData = useFindDataById(id, contestApiValue);
+
+  console.log(foundDonationData);
+
   return <div>DonationDetails id is : {id} </div>;
 }
 
